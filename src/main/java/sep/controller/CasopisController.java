14719b16f0a1f5f2a7686132307c5116bb8a91aa
Marko1808 <malimrav119>
2018-12-04 00:32:49 +0100

    Placanje karticom

--- src/main/java/sep/controller/CasopisController.java
+++ src/main/java/sep/controller/CasopisController.java
@@ -26,62 +26,81 @@ import sep.service.CasopisService;
 @RestController
 @RequestMapping(value = "/casopis")
 public class CasopisController {
-	
+
 	@Autowired
 	private CasopisService casopisService;
-	
 
-	@RequestMapping(
-			value = "/getCasopisi",
-			method = RequestMethod.GET
-	)
+	@RequestMapping(value = "/getCasopisi", method = RequestMethod.GET)
 	public ResponseEntity<?> getCasopisi() {
 		List<Casopis> listaCasopisa = casopisService.getAll();
 		ArrayList<CasopisDTO> listaCasopisaDTO = new ArrayList<CasopisDTO>();
-		for(Casopis c : listaCasopisa) {
+		for (Casopis c : listaCasopisa) {
 			CasopisDTO casopis = new CasopisDTO(c);
 			listaCasopisaDTO.add(casopis);
 		}
 		return new ResponseEntity<>(listaCasopisaDTO, HttpStatus.OK);
 	}
-	
-	@RequestMapping(
-			value = "/proveriMerchanta",
-			method = RequestMethod.POST
-	)
+
+	@RequestMapping(value = "/proveriMerchanta", method = RequestMethod.POST)
 	public boolean proveriMerchanta(@RequestBody MerchantDTO casopis) {
 		List<Casopis> listaCasopisa = casopisService.getAll();
-		
-		for(Casopis c : listaCasopisa) {
-			if(c.getMerchant_id().equals(casopis.getMerchant_id()) && c.getMerchant_password().equals(casopis.getMerchant_password())){
+
+		for (Casopis c : listaCasopisa) {
+			if (c.getMerchant_id().equals(casopis.getMerchant_id())
+					&& c.getMerchant_password().equals(casopis.getMerchant_password())) {
 				return true;
 			}
 		}
 		return false;
-		
+
 	}
-	
+
 	@CrossOrigin
-	@RequestMapping(
-			value = "/kupiCasopis",
-			method = RequestMethod.POST
-	)
+	@RequestMapping(value = "/kupiCasopis", method = RequestMethod.POST)
 	public ResponseEntity<?> kupiCasopis(@RequestBody CardDTO casopis) {
 		System.out.println("Stao tu");
 		URLDTO urlDTO = new URLDTO();
-		RestTemplate client = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        try {
-        	headers.setContentType(MediaType.APPLICATION_JSON);
-            HttpEntity<CardDTO> entity = new HttpEntity<>(casopis, headers);
-            urlDTO = client.postForObject("http://localhost:1235/payment/izvrsiPlacanje", entity,
-            		URLDTO.class);
-            return new ResponseEntity<>(urlDTO, HttpStatus.OK);
-        } catch (Exception e) {
-            System.out.println("Ne moze da posalje");
-            return null; 
-        }	
+		RestTemplate client1 = new RestTemplate();
+		HttpHeaders headers1 = new HttpHeaders();
+		CardDTO karticaCasopisa = new CardDTO();
+		try {
+			headers1.setContentType(MediaType.APPLICATION_JSON);
+			HttpEntity<CardDTO> entity = new HttpEntity<>(casopis, headers1);
+			karticaCasopisa = client1.postForObject("http://localhost:1235/payment/findCard", entity, CardDTO.class);
+
+		} catch (Exception e) {
+			System.out.println("Ne moze da dobijeKarticu");
+
+		}
+		String kupacBanka = casopis.getPan().substring(1, 7);
+		String casopisBanka = karticaCasopisa.getPan().substring(1, 7);
+
+		if (kupacBanka.equals(casopisBanka)) {
+
+			RestTemplate client = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			try {
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				HttpEntity<CardDTO> entity = new HttpEntity<>(casopis, headers);
+				urlDTO = client.postForObject("http://localhost:1235/payment/izvrsiPlacanje", entity, URLDTO.class);
+				return new ResponseEntity<>(urlDTO, HttpStatus.OK);
+			} catch (Exception e) {
+				System.out.println("Ne moze da posalje");
+				return null;
+			}
+		}else {
+			RestTemplate client = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			try {
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				HttpEntity<CardDTO> entity = new HttpEntity<>(casopis, headers);
+				urlDTO = client.postForObject("http://localhost:1237/platneKartice/usmeriNaBanku", entity, URLDTO.class);
+				return new ResponseEntity<>(urlDTO, HttpStatus.OK);
+			} catch (Exception e) {
+				System.out.println("Ne moze da posalje");
+				return null;
+			}
+		}
 	}
-	
 
 }
